b8a20a625a912a769741a28e4c5760003cb735fd
James Ward <j.ward@acfr.usyd.edu.au>
2013-10-02 14:05:33 +1000

    RobotMap.java ported to C++.

---
+++ src/RobotMap.cpp
@@ -0,0 +1,63 @@
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+
+#include "RobotMap.h"
+
+// Initialise all of the peripherals
+RobotMap::RobotMap():
+    // Joysticks
+    joyDrv(new Joystick(1)),
+    joyOpr(new Joystick(2)),
+    // Joystick dead zones. Default 0.05
+    JOY_DRV_DEAD_X(0.05),
+    JOY_DRV_DEAD_Y(0.05),
+    JOY_OPR_DEAD_X(0.05),
+    JOY_OPR_DEAD_Y(0.05),
+    // Servo angles
+    OPEN_SERVO_ANGLE(30.0),
+    CLOSE_SERVO_ANGLE(0.0),
+    FIRE_PIN_SPEED(-0.5),
+    // Joystick buttons
+    fireButton(new JoystickButton(joyOpr, 6)),
+    slowButton(new JoystickButton(joyOpr, 7)),
+    fastButton(new JoystickButton(joyOpr, 8)),
+    stopButton(new JoystickButton(joyOpr, 1)),
+    topButton(new JoystickButton(joyOpr, 4)),
+    bottomButton(new JoystickButton(joyOpr, 2)),
+    // Shooter motors
+    firePinMotor(new Victor(5)),
+    shooterMotorFront(new Victor(4)),
+    shooterMotorBack(new Victor(6)),
+
+    // Hopper servos
+    hopperServoTop(new Servo(8)),
+    hopperServoBottom(new Servo(7)),
+
+    // Sensors
+    // Firing pin limit switches
+    firePinMax(new DigitalInput(8)),
+    firePinMin(new DigitalInput(7))
+{}
+
+RobotMap::~RobotMap() {
+    // Delete all of the instances of our peripherals
+    delete joyDrv;
+    delete joyOpr;
+    delete fireButton;
+    delete slowButton;
+    delete fastButton;
+    delete stopButton;
+    delete topButton;
+    delete bottomButton;
+    delete firePinMotor;
+    delete shooterMotorFront;
+    delete shooterMotorBack;
+    delete hopperServoTop;
+    delete hopperServoBottom;
+    delete firePinMax;
+    delete firePinMin;
+}
---
+++ src/RobotMap.h
@@ -0,0 +1,62 @@
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+
+#ifndef RobotMap_h
+#define RobotMap_h
+
+#include "WPILib.h"
+
+class RobotMap {
+    public:
+        // Methods
+        RobotMap(); // Constructor
+        ~RobotMap(); // Destructor
+        // Members
+        // Joysticks
+        Joystick* const joyDrv;
+        Joystick* const joyOpr;
+        
+        // Joystick dead zones
+        const double JOY_DRV_DEAD_X;
+        const double JOY_DRV_DEAD_Y;
+        const double JOY_OPR_DEAD_X;
+        const double JOY_OPR_DEAD_Y;
+
+        // Servo angles
+        const double OPEN_SERVO_ANGLE;
+        const double CLOSE_SERVO_ANGLE;
+        const double FIRE_PIN_SPEED;
+  
+        // Button definitions
+        Button* const fireButton;
+        Button* const slowButton;
+        Button* const fastButton;
+        Button* const stopButton;
+        Button* const topButton;
+        Button* const bottomButton;
+
+        // Actuator definitions
+  
+        // Shooter motors
+        Victor* const firePinMotor;
+        Victor* const shooterMotorFront;
+        Victor* const shooterMotorBack;
+  
+        // Hopper servos
+        Servo* const hopperServoTop;
+        Servo* const hopperServoBottom;
+  
+        // Sensors
+        // Firing pin limit switches
+        DigitalInput* const firePinMax;
+        DigitalInput* const firePinMin;
+    
+    private:
+        // No private members    
+};
+
+#endif
